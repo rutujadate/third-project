0221a313f6a5690a424d21419f7abd832cee15d0
rutuja.date <daterutuja5646@gmail.com>
2025-08-25 15:27:13 +0530

    Student information display in table

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const object={
-    Name:'Rutuja',
-    Age:20,
-    College:'B.J.College Ale'
-  
-  }
+  const student = [
+    { srno: 1, name: 'Rutuja', marks: 90 },
+    { srno: 2, name: 'Shravani', marks: 90 },
+    { srno: 3, name: 'Trusha', marks: 90 },
+    { srno: 4, name: 'Gaurav', marks: 90 },
+    { srno: 5, name: 'Shardul', marks: 90 },
+    { srno: 6, name: 'Anamika', marks: 90 },
+    { srno: 7, name: 'Ayush', marks: 90 },
+
+  ]
   return (
-    <div>
-      <div><strong>Name:</strong>{object.Name}</div>
-      <div><strong>Age:</strong>{object.Age}</div>
-      <div><strong>College:</strong>{object.College}</div>
-    </div>
+    <table border='1'>
+      <tr>
+        <th>srno</th>
+        <th>name</th>
+        <th>marks</th>
+      </tr>
+      {student.map((singleElement) => {
+        return <tr>
+          <td>{singleElement.srno}</td>
+          <td>{singleElement.name}</td>
+          <td>{singleElement.marks}</td>
+        </tr>
+        
+   
+  })}
+    </table>
   );
 }
 
